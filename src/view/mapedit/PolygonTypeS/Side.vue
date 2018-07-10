9b5eecb5507c3163c0195f041e6eccb58c1485be
lixingcheng <lixingcheng@kingtopware.com>
2018-07-10 08:58:04 +0800

    feat: 图层编辑lixingcheng

---
+++ src/view/mapedit/PolygonTypeS/Side.vue
@@ -0,0 +1,96 @@
+
+<script>
+export default {
+  name: 'Side',
+  data() {
+    return {
+      showSide: true,
+      colors: 'black',
+    };
+  },
+  methods: {
+    change(status) {
+      this.$Message.info('开关状态：' + status);
+    },
+  },
+};
+</script>
+
+<template>
+  <div>
+    <p class="p">
+      <span>显示/不显示 边</span>
+      <Switch
+        v-model="showSide"
+        @on-change="change"></Switch>
+    </p>
+    <p class="p">
+      <span>宽度:</span>
+      <InputNumber
+        :max="100"
+        :min="1"
+        style="width: 120px"
+      ></InputNumber>
+      <span>依据字段：<span>
+        <Select
+          style="width:150px">
+        </Select>
+    </span></span></p>
+    <p class="p">
+      <span>透明度:</span>
+      <InputNumber
+        :max="100"
+        :min="0"
+        :step="10"
+        :formatter="value => `${value}%`"
+        :parser="value => value.replace('%', '')"
+        style="width: 328px"
+      ></InputNumber>
+    </p>
+    <p class="p">
+      <span>颜色修改:</span>
+      <ColorPicker
+        v-model="colors"
+        size="large"
+        class="colorsStyle" />
+      <span>依据字段：<span>
+        <Select
+          style="width:208px">
+        </Select>
+    </span></span></p>
+    <p class="p">
+      <span>虚线:</span>
+      <InputNumber
+        :max="1000"
+        :min="-1000"
+        style="width: 342px"></InputNumber>
+    </p>
+    <p class="p">
+      <span>虚线偏移:</span>
+      <InputNumber
+        :max="1000"
+        :min="-1000"
+        style="width: 317px"
+      ></InputNumber>
+    </p>
+    <p class="p">
+      <span>线帽:</span>
+      <Select
+        style="width:342px">
+      </Select>
+    </p>
+    <p class="p">
+      <span>线连接:</span>
+      <Select
+        style="width:329px">
+      </Select>
+    </p>
+
+  </div>
+</template>
+
+<style lang="less" scoped>
+.p {
+  margin: 8px 0 0 0;
+}
+</style>
---
+++ src/view/mapedit/PolygonTypeS/Tagging.vue
@@ -0,0 +1,64 @@
+
+<script>
+export default {
+  name: 'Tagging',
+  data() {
+    return {
+      showSide: true,
+      colors: 'black',
+    };
+  },
+  methods: {
+    change(status) {
+      this.$Message.info('开关状态：' + status);
+    },
+  },
+};
+</script>
+
+<template>
+  <div>
+    <p class="p">
+      <span>显示/不显示 标注</span>
+      <Switch
+        v-model="showSide"
+        @on-change="change"></Switch>
+    </p>
+    <p class="p">
+      <span>标注:</span>
+      <Input
+        placeholder="Enter something..."
+        style="width: 170px"></Input>
+      <Select
+        style="width:165px">
+      </Select>
+    </p>
+    <p class="p">
+      <span>透明度:</span>
+      <InputNumber
+        :max="100"
+        :min="0"
+        :step="10"
+        :formatter="value => `${value}%`"
+        :parser="value => value.replace('%', '')"
+        style="width: 326px"
+      ></InputNumber>
+    </p>
+    <p class="p">
+      <span>旋转:</span>
+      <InputNumber
+        :max="360"
+        :min="-360"
+        :step="45"
+        style="width: 326px"
+      ></InputNumber>
+    </p>
+
+  </div>
+</template>
+
+<style lang="less" scoped>
+.p {
+  margin: 8px 0 0 0;
+}
+</style>
