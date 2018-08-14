3edc97538bfc01dde4c8865cb9b897649ef0f938
limeng <386784901@qq.com>
2018-08-14 17:29:18 +0800

    fix: 编写树查询

--- src/view/business/BusinessSort.vue
+++ src/view/business/BusinessSort.vue
@@ -5,89 +5,15 @@ export default {
     return {
       dataTree: [
         {
-          title: '文件夹',
+          title: '',
           expand: true,
-          render: (h, { root, node, data }) => {
-            return h(
-              'span',
-              {
-                style: {
-                  display: 'inline-block',
-                  width: '100%',
-                },
-              },
-              [
-                h('span', [
-                  h('Icon', {
-                    props: {
-                      type: 'ios-folder',
-                    },
-                    style: {
-                      marginRight: '8px',
-                      fontSize: '10px',
-                      color: '#ff9900',
-                    },
-                  }),
-                  h('span', data.title),
-                ]),
-                h(
-                  'span',
-                  {
-                    style: {
-                      display: 'inline-block',
-                      float: 'right',
-                      marginRight: '32px',
-                    },
-                  }[
-                    h('Button', {
-                      props: Object.assign({}, this.ButtonProps, {
-                        icon: 'ios-plus-empty',
-                        // type: 'primary',
-                      }),
-                      style: {
-                        marginRight: '5px',
-                        padding: '0px 3px',
-                      },
-                      on: {
-                        click: () => {
-                          this.append(data);
-                        },
-                      },
-                    })
-                  ]
-                ),
-              ]
-            );
-          },
           children: [
             {
-              title: '文件夹一',
+              title: '',
               expand: true,
               children: [
                 {
-                  title: '数据1',
-                  expand: true,
-                },
-                {
-                  title: '数据2',
-                  expand: true,
-                },
-              ],
-            },
-            {
-              title: '文件夹二',
-              expand: true,
-              children: [
-                {
-                  title: '数据1',
-                  expand: true,
-                },
-                {
-                  title: '数据2',
-                  expand: true,
-                },
-                {
-                  title: '数据3',
+                  title: '',
                   expand: true,
                 },
               ],
@@ -95,185 +21,23 @@ export default {
           ],
         },
       ],
-      ButtonProps: {
-        type: 'default',
-        size: 'small',
-      },
+      // ButtonProps: {
+      //   type: 'default',
+      //   size: 'small',
+      // },       :render="renderContent"
     };
   },
-  methods: {
-    renderContent(h, { root, node, data }) {
-      return h(
-        'span',
-        {
-          style: {
-            display: 'inline-block',
-            width: '100%',
-          },
-        },
-        [
-          h(
-            'span',
-            {
-              style: {
-                width: '85px',
-                background: 'none',
-              },
-            },
-            [
-              h('Icon', {
-                props: {
-                  type: 'ios-paper-outline',
-                },
-                style: {
-                  marginRight: '5px',
-                },
-              }),
-              h(
-                'k-input',
-                {
-                  props: {
-                    size: 'small',
-                    value: data.title,
-                    // readonly: true,
-                    // disabled: true,
-                  },
-                  style: {
-                    width: '65px',
-                  },
-                },
-                data.title
-              ),
-            ]
-          ),
-          h(
-            'span',
-            {
-              style: {
-                display: 'inline-block',
-                float: 'right',
-                marginRight: '10px',
-              },
-            },
-            [
-              h('Button', {
-                props: Object.assign({}, this.ButtonProps, {
-                  icon: 'ios-plus-empty',
-                }),
-                style: {
-                  marginRight: '5px',
-                  padding: '0px 3px',
-                },
-                on: {
-                  click: () => {
-                    this.append(data);
-                  },
-                },
-              }),
-              h('Button', {
-                props: Object.assign({}, this.ButtonProps, {
-                  icon: 'ios-minus-empty',
-                }),
-                style: {
-                  marginRight: '5px',
-                  padding: '0px 3px',
-                },
-                on: {
-                  click: () => {
-                    this.remove(root, node, data);
-                  },
-                },
-              }),
-              h('Button', {
-                props: Object.assign({}, this.ButtonProps, {
-                  icon: 'ios-compose-outline',
-                }),
-                style: {
-                  padding: '0px 3px',
-                },
-                on: {
-                  click: () => {
-                    this.edit(root, node, data);
-                  },
-                },
-              }),
-            ]
-          ),
-        ]
-      );
-    },
-    append(data) {
-      this.$Modal.confirm({
-        render: h => {
-          return h('Input', {
-            props: {
-              value: this.value,
-              autofocus: true,
-              placeholder: '请输入分类名',
-            },
-            on: {
-              input: val => {
-                this.value = val;
-              },
-            },
-            onOk: () => {
-              this.$Message.info('Clicked ok');
-            },
-            onCancel: () => {
-              this.$Message.info('Clicked cancel');
-            },
-          });
-        },
-      });
-
-      // const children = data.children || [];
-
-      // children.push({
-      //   title: '数据',
-      //   expand: true,
-      // });
-      // this.$set(data, 'children', children);
-    },
-    remove(root, node, data) {
-      //获取根节点
-      const parentKey = root.find(el => el === node).parent;
-      //获取父节点
-      const parent = root.find(el => el.nodeKey === parentKey).node;
-      //获取当前节点
-      const index = parent.children.indexOf(data);
-      parent.children.splice(index, 1);
-    },
-    edit(root, node, data) {
-      this.$Modal.confirm({
-        render: h => {
-          return h('Input', {
-            props: {
-              value: this.value,
-              autofocus: true,
-              placeholder: '请输入分类名',
-            },
-            on: {
-              input: val => {
-                this.value = val;
-              },
-            },
-          });
-        },
-      });
-    },
-    // //获取根节点
-    // const parentKey = root.find(el => el === node).parent;
-    //  //获取父节点
-    // const parent = root.find(el => el.nodeKey === parentKey).node;
-    // // //获取当前节点
-    // const index = parent.children.indexOf(data);
-    //  //获取当前节点的名称
-    //  parent.children[index].title;
+  mounted() {
+    this.searchTree();
   },
-  // },
+  methods: {
+    async searchTree() {
+      const response = await api.db.findalltype({});
+      this.dataTree = response.data;
+    },
+  },
 };
 </script>
-
 <template>
   <div class="main">
     <div
@@ -285,7 +49,8 @@ export default {
       <span>资源分类</span>
       <Tree
         :data="dataTree"
-        :render="renderContent"></Tree>
+
+      ></Tree>
 </div></div></template>
 
 <style lang="less" scoped>
