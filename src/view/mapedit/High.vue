ca18d0f09578ab0baecdf56795d47520f2c44a9c
huangge <huangge@kingtopware.com>
2018-08-18 09:48:49 +0800

    fix: 处理冲突

--- src/view/mapedit/High.vue
+++ src/view/mapedit/High.vue
@@ -153,7 +153,6 @@ export default {
           值: value[1],
         });
       }
-      debugger;
     },
     getColor() {
       var param = {
@@ -161,14 +160,6 @@ export default {
         statisticsFields: [{ field: 'gid', operate: 'count' }],
         typename: 'ktw:' + this.msg.data.name,
       };
-<<<<<<< HEAD
-      param = { statistics: JSON.stringify(param) };
-      console.log(param);
-      param = { statistics: JSON.stringify(param) };
-      console.log(param);
-      // param = { statistics: JSON.stringify(param) };
-=======
->>>>>>> a70fcc24e952802fffd2b4f698d46e771a606439
       L.ajax({
         url: `${config.project.highgisUrl}/hgis/ows?service=wps&request=aggregate`,
         success: this.statisticsSuccess,
