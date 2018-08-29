b03d21753351ea11756928f3a92f965718049658
lixingcheng <lixingcheng@kingtopware.com>
2018-08-29 09:12:51 +0800

    fix: 表单验证规则

--- src/view/business/tagdetails/rule-validate.js
+++ src/view/business/tagdetails/rule-validate.js
@@ -1,7 +1,7 @@
 const RuleValidate = {
   name: [{ required: true, message: '该项不能为空', trigger: 'blur' }],
   restitle: [{ required: true, message: '业务资源标题不能为空', trigger: 'blur' }],
-  restype: [{ required: true, message: '资源分类不能为空', trigger: 'blur' }],
+  restype: [{ required: true, message: '资源分类不能为空', trigger: 'change' }],
   keyword: [
     {
       required: true,
@@ -12,8 +12,11 @@ const RuleValidate = {
     },
   ],
 
-  type: [{ required: true, message: '字段类型不能为空', trigger: 'blur' }],
+  type: [{ required: true, message: '字段类型不能为空', trigger: 'change' }],
   length: [{ required: true, message: '字段长度不能为空', trigger: 'blur' }],
   relatedtype: [{ required: true, message: 'Please select gender', trigger: 'change' }],
+  name: [{ required: true, message: '该项不能为空', trigger: 'blur' }],
+  classfiy: [{ required: true, message: '字段分类不能为空', trigger: 'change' }],
+  extendmethod: [{ required: true, message: '处理规则不能为空', trigger: 'blur' }],
 };
 export default RuleValidate;
