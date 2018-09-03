590e8980ed4649a296ea008bf87113a764503b13
yangchao <yangchao@kingtopware.com>
2018-09-03 14:45:54 +0800

    fix: 修复元数据上传BUG

--- src/components/extractplan/FilterResource.vue
+++ src/components/extractplan/FilterResource.vue
@@ -3,7 +3,7 @@ import * as helps from '@/utils/helps';
 import AttributeFilter from './AttributeFilter';
 
 //提取必须字段
-const REQUIRED_FIELDS = ['gid', 'x1', 'y1', 'x2', 'y2', 'Shape_Leng', 'Shape_Area', 'bbox'];
+const REQUIRED_FIELDS = ['Shape_Leng', 'Shape_Area'];
 
 /*
  * 过滤提取资源模块
@@ -42,10 +42,10 @@ export default {
       indeterminate: false,
       //是否已全选
       checkAll: false,
+      //字段列表
+      schemas: [],
       //字段选择列表
       selectSchemas: [],
-      //保留字段列表
-      reservedSchemas: [],
     };
   },
   created() {
@@ -57,9 +57,8 @@ export default {
       let layer = this.selectLayers[index];
       //查询资源schema信息
       const response = await api.db.findResourceInfo({ id: layer.resid });
-      let fields = response.data.schema.map(p => p.name);
-      this.reservedSchemas = fields.filter(p => REQUIRED_FIELDS.indexOf(p) > 0);
-      this.currentSchemas = helps.filterSchema(fields);
+      this.schemas = response.data.schema.map(p => p.name);
+      this.currentSchemas = helps.filterSchema(this.schemas);
       this.currentFilter = {
         schemas: this.currentSchemas.join(','),
         name: layer.resname,
@@ -112,11 +111,17 @@ export default {
     //应用当前更改
     applyCurrentChange() {
       let curLayer = this.selectLayers.find(p => p.resid == this.currentId);
-      curLayer.schema = [...this.selectSchemas, ...this.reservedSchemas].join(',');
+      curLayer.schema = this.getSelectSchemas().join(',');
       let filter = this.$refs.filterEditor.getFilter();
       if (filter) {
         curLayer.filter = filter;
       }
+    },
+    //获取选择的shemas
+    getSelectSchemas() {
+      let fields = [...this.selectSchemas, ...REQUIRED_FIELDS];
+      fields = this.schemas.filter(p => fields.indexOf(p) >= 0);
+      return fields;
     },
     //字段全选
     handleCheckAll() {
